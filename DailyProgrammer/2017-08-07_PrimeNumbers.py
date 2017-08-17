7a8d8c9736f76f881f30402af3c6b55611142524
Travis Munson <munson.t@gmail.com>
2017-08-17 09:17:08 -0500

    print input if prime

--- DailyProgrammer/2017-08-07_PrimeNumbers.py
+++ DailyProgrammer/2017-08-07_PrimeNumbers.py
@@ -4,12 +4,12 @@ import math
 
 input_integer = input("Enter an integer: ")
 
-boolean_list = [True for i in range(2,input_integer)]
+boolean_list = [True for i in range(2,input_integer+1)]
 for i in range(2,int(math.sqrt(input_integer)+1)):
     if boolean_list[i] is True:
         j = 0
-        for num in range(0,input_integer):
-            if input_integer > i**2+num*i:
+        for num in range(0,input_integer+1):
+            if input_integer+1 > i**2+num*i:
                 j = i**2+num*i
                 #print j
                 boolean_list[j-2] = False
@@ -17,4 +17,7 @@ for i in range(2,int(math.sqrt(input_integer)+1)):
 #print boolean_list
 primes = [i+2 for i in range(0,len(boolean_list)) if boolean_list[i] == True]
 # The highest prime below a given integer
-print primes[-1]
+if primes[-1] == input_integer:
+	print "%d is a prime number." % input_integer
+else:
+	print primes[-1]
